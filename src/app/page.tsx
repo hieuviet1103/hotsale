c01763e1794815f771b9e972480f78546f0d46be
hieuviet1103 <hieuviet.1103@gmail.com>
2024-06-25 20:39:57 +0700

    modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/layout.tsx 	modified:   src/app/page.module.css 	modified:   src/app/page.tsx 	deleted:    src/presentation/scss/index.scss 	deleted:    src/presentation/scss/pages/home/home.scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styleHome from "@@styles/pages/home/home.module.scss";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-          
-        </p>
-        <h1>From Hieunv.dev</h1>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styleHome["main"]}>
+      <div className={styleHome["voucher-dealcoupon"]}>
+        <div className={styleHome["title-tabs"]}>
+          <div className={styleHome["title-tabs--name"]}>
+            <span>Danh sách mã khuyến mãi</span>
+          </div>
+          <div className={styleHome["title-tabs--type"]}>
+            <div className={styleHome["title-tabs--type-item"]}>
+              <span>Đang mở</span>
+            </div>{" "}
+            <div className={styleHome["title-tabs--type-item"]}>
+              <span>Sắp mở</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styleHome["tab-content"]}>
+          <div className={styleHome["dealpromo-body"]}>
+            <div className={styleHome["dealpromo-item"]}></div>
+
+            <div className={styleHome["dealpromo-item"]}></div>
+
+            <div className={styleHome["dealpromo-item"]}></div>
+
+            <div className={styleHome["dealpromo-item"]}></div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Home;
